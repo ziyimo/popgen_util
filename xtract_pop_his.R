dd63cda22c2712f9403790b17bb1611397a2df59
Ziyi Mo <mo@the-machine.cshl.edu>
2019-04-19 17:49:50 -0400

    Added an option to specify the maximum number of generations of population history to be extracted when using `xtract_pop_his.R`

--- xtract_pop_his.R
+++ xtract_pop_his.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 cmd_args <- commandArgs(trailingOnly = TRUE)
 
-get_Ne_by_gen<-function(psmc_file,it=30,mu=2.5e-8,s=100){
+get_Ne_by_gen<-function(psmc_file,it=30,mu=2.5e-8,s=100,maxGBP=2e5){
   X<-scan(file=psmc_file,what="",sep="\n",quiet=TRUE)
   
   START<-grep("^RD",X)
@@ -26,11 +26,17 @@ get_Ne_by_gen<-function(psmc_file,it=30,mu=2.5e-8,s=100){
   Generation<-round(as.numeric(2*N0*a[,3]))
   Ne<-round(as.numeric(N0*a[,4]))
   
+  mask <- Generation < maxGBP
+  Generation<-Generation[mask]
+  Ne<-Ne[mask]
+  
   file.remove("temp.psmc.result")
   
-  goi <- rev(Generation[Ne!=lag(Ne)][-1])
-  goi <- max(goi) - goi
-  dNe <- rev(Ne[Ne!=lead(Ne)])[-1]
+  goi <- c(maxGBP, rev(Generation[Ne!=lag(Ne)][-1]), 0)
+  dNe <- c(tail(Ne, n=1), rev(Ne[Ne!=lead(Ne)])[-1], Ne[1])
+  
+  goi <- maxGBP - goi
+  
   gen_Ne <- cbind(goi, dNe)
   #data.frame(round(Generation),round(Ne))
   return(list(c(mu, r), gen_Ne))
@@ -38,13 +44,14 @@ get_Ne_by_gen<-function(psmc_file,it=30,mu=2.5e-8,s=100){
 
 main <- function(arguments){
 
-  if(length(arguments) != 2){
-    cat("Usage: $ ./xtract_pop_his.R <psmc_file> <out_file>", "\n")
+  if(length(arguments) != 3){
+    cat("Usage: $ ./xtract_pop_his.R <psmc_file> <out_file> <max_gen>", "\n")
     return(-1)
   }
-  usr_args <- tail(arguments, 2)
+  #usr_args <- tail(arguments, 3)
+  usr_args <- arguments
   
-  params <- get_Ne_by_gen(usr_args[1])
+  params <- get_Ne_by_gen(usr_args[1], maxGBP=strtoi(usr_args[3]))
   write.table(params[[1]], file = usr_args[2], row.names = FALSE, col.names = FALSE)
   write.table(params[[2]], file = usr_args[2], append = TRUE, row.names = FALSE, col.names = FALSE)
   
